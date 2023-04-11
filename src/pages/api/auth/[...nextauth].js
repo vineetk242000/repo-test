d8c032c7faf37f463144ba0c75d50bfa59e76585
SAURABH KUMAR <hmsaurabhkr@gmail.com>
2023-04-12 01:24:54 +0530

    added coustom mail for verify and welcome

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,25 +5,76 @@ import nodemailer from 'nodemailer';
 
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 // Instantiate Prisma Client
 const prisma = new PrismaClient();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+  secure: true,
+});
+
+
+// Verify custom email
+const emailsDir = path.resolve(process.cwd(), 'emails');
+const sendVerificationRequest = ({ identifier, url }) => {
+  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+    encoding: 'utf8',
+  });
+  const emailTemplate = Handlebars.compile(emailFile);
+  transporter.sendMail({
+    from: `"✨ RentHouse" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for RentHouse',
+    html: emailTemplate({
+      base_url: process.env.NEXTAUTH_URL,
+      signin_url: url,
+      email: identifier,
+    }),
+  });
+};
+
+
+// Welcome email
+const sendWelcomeEmail = async ({ user }) => {
+  const { email } = user;
+
+  try {
+    const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+      encoding: 'utf8',
+    });
+    const emailTemplate = Handlebars.compile(emailFile);
+    await transporter.sendMail({
+      from: `"✨ RentHouse" ${process.env.EMAIL_FROM}`,
+      to: email,
+      subject: 'Welcome to RentHouse! 🎉',
+      html: emailTemplate({
+        base_url: process.env.NEXTAUTH_URL,
+        support_email: 'hmsaurabhkr@gmail.com',
+      }),
+    });
+  } catch (error) {
+    console.log(`❌ Unable to send welcome email to user (${email})`);
+  }
+};
+
 
 // pages/api/auth/[...nextauth].js
 export default NextAuth({
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
       maxAge: 10 * 60, // Magic links are valid for 10 min only
+      sendVerificationRequest,
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  events: { createUser: sendWelcomeEmail }
 });
